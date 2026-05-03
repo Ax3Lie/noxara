eefb12ebd94bdb4f2553276dc16d091c7f62680d
Denys Khudolii <seethefleece@gmail.com>
2026-05-03 16:16:27 +0100

    Quartz sync: May 3, 2026, 4:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ax3lie.github.io/noxara",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
